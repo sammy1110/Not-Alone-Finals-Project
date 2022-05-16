c8e5c7c2041df45200a655635840b166b2bdef8c
Roman <sammy10roman@gmail.com>
2022-05-16 12:38:34 -0700

    UI
    Buttons

--- NotAloneFinalProject/Assets/Scripts/MainMenu.cs
+++ NotAloneFinalProject/Assets/Scripts/MainMenu.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+   
 }
