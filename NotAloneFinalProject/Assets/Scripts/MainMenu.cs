92797694dc23c39b52e018008a8dbd4e58133bba
Diaz <701708@student.dusd.net>
2022-05-16 12:47:29 -0700

    Sprites import
    Lz4 spritesheets and music import

--- NotAloneFinalProject/Assets/Scripts/MainMenu.cs
+++ NotAloneFinalProject/Assets/Scripts/MainMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.TMPro;
+//using UnityEngine.TMPro;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
