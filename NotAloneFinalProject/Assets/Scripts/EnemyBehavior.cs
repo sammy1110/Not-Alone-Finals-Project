52c4b96a153a220d48ddcb5dc0a63642ebdc9312
Roman <sammy10roman@gmail.com>
2022-05-25 16:04:57 -0700

    stuff
    anim

--- NotAloneFinalProject/Assets/Scripts/EnemyBehavior.cs
+++ NotAloneFinalProject/Assets/Scripts/EnemyBehavior.cs
@@ -4,17 +4,64 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public float speed;
+    public bool vertical;
+
+    public float changeTime = 3.0f;
+    float timer;
+    int direction = 1;
 
     public float Hitpoints;
     public float MaxiHitpoints = 5;
+
+    Rigidbody2D rigidbody2d;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         Hitpoints = MaxiHitpoints;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        timer = changeTime;
     }
 
-   public void TakeHit(float damage)
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+
+
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move Y", 0);
+            animator.SetFloat("Move X", direction);
+        }
+
+        rigidbody2d.MovePosition(position);
+    }
+
+
+    public void TakeHit(float damage)
     {
         Hitpoints -= damage;
         if(Hitpoints <= 0)
